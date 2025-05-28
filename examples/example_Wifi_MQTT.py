29be2fa9d6fb77ea3edc94126c840ebb45c58cff
Akarapon1909 <akarapon.the@gmail.com>
2025-05-28 14:23:44 +0700

    v.2

--- examples/example_Wifi_MQTT.py
+++ examples/example_Wifi_MQTT.py
@@ -7,18 +7,22 @@ def publish_example():
     print("Publishing to MQTT...")
     wifi_mqtt.publish("smartfarm/status", "Online")
 
-wifi_mqtt = WiFiMQTTClient(
-    ssid="YourSSID",
-    password="YourPassword",
-    mqtt_broker="broker.hivemq.com",
-    mqtt_port=1883,
-    mqtt_client="esp32_client_001",
-    mqtt_user="",
-    mqtt_pass="",
-    topic_sub=b"smartfarm/command",
-    callback=mqtt_callback
-)
+def main():
+    wifi_mqtt = Wifi(
+        ssid="YourSSID",
+        password="YourPassword",
+        mqtt_broker="broker.hivemq.com",
+        mqtt_port=1883,
+        mqtt_client="esp32_client_001",
+        mqtt_user="",
+        mqtt_pass="",
+        topic_sub=b"smartfarm/command",
+        callback=mqtt_callback
+    )
 
-wifi_mqtt.connect_wifi()
-wifi_mqtt.connect_mqtt()
-wifi_mqtt.auto_loop(publish_callback=publish_example, interval_ms=10000)
+    wifi_mqtt.connect_wifi()
+    wifi_mqtt.connect_mqtt()
+    wifi_mqtt.auto_loop(publish_callback=publish_example, interval_ms=10000)
+
+if __name__ == "__main__":
+    main()
--- examples/example_button.py
+++ examples/example_button.py
@@ -1,9 +1,12 @@
 from ETSmartFarm import Button
 import time
 
-btn_reader = I2CButtonReader()
+def main():
+    btn_reader = Button()
+    while True:
+        states = btn_reader.read_buttons()
+        print("Pin states:", states)
+        time.sleep(0.5)
 
-while True:
-    states = btn_reader.read_buttons()
-    print("Pin states:", states)
-    time.sleep(0.5)
+if __name__ == "__main__":
+    main()
